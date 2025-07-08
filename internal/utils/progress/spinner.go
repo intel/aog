e41328ba46c5056b296b394d351d452c92bf181b
Jonathan Ding <jonathan.ding@intel.com>
2025-07-08 10:25:06 +0800

    v0.4.0 preview

--- internal/utils/progress/spinner.go
+++ internal/utils/progress/spinner.go
@@ -1,99 +1,10 @@
-//*****************************************************************************
-// Copyright 2025 Intel Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//*****************************************************************************
-
 package progress
 
 import (
 	"fmt"
-	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 )
-
-type Spinner struct {
-	message      atomic.Value
-	messageWidth int
-
-	parts []string
-
-	value int
-
-	ticker  *time.Ticker
-	started time.Time
-	stopped time.Time
-}
-
-func NewSpinner(message string) *Spinner {
-	s := &Spinner{
-		parts: []string{
-			"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏",
-		},
-		started: time.Now(),
-	}
-	s.SetMessage(message)
-	go s.start()
-	return s
-}
-
-func (s *Spinner) SetMessage(message string) {
-	s.message.Store(message)
-}
-
-func (s *Spinner) String() string {
-	var sb strings.Builder
-
-	if message, ok := s.message.Load().(string); ok && len(message) > 0 {
-		message := strings.TrimSpace(message)
-		if s.messageWidth > 0 && len(message) > s.messageWidth {
-			message = message[:s.messageWidth]
-		}
-
-		fmt.Fprintf(&sb, "%s", message)
-		if padding := s.messageWidth - sb.Len(); padding > 0 {
-			sb.WriteString(strings.Repeat(" ", padding))
-		}
-
-		sb.WriteString(" ")
-	}
-
-	if s.stopped.IsZero() {
-		spinner := s.parts[s.value]
-		sb.WriteString(spinner)
-		sb.WriteString(" ")
-	}
-
-	return sb.String()
-}
-
-func (s *Spinner) start() {
-	s.ticker = time.NewTicker(100 * time.Millisecond)
-	for range s.ticker.C {
-		s.value = (s.value + 1) % len(s.parts)
-		if !s.stopped.IsZero() {
-			return
-		}
-	}
-}
-
-func (s *Spinner) Stop() {
-	if s.stopped.IsZero() {
-		s.stopped = time.Now()
-	}
-}
 
 var animations = [][]rune{
 	{'▁', '▃', '▄', '▅', '▆', '▇', '█', '▇', '▆', '▅', '▄', '▃', '▁'},
